8f89ec02fc7573427b0d776cb9bda107a2b19473
SAL <heso482485@gmail.com>
2023-12-20 15:06:55 +0200

    implemented the push back function in the Vector Class

--- Vector.cpp
+++ Vector.cpp
@@ -11,19 +11,25 @@ class Vector {
             capacity = ceil(size * 2);
             int *newVector = nullptr;
             newVector = new int[capacity];
+            for(int i = 0; i < size; i++) {
+                newVector[i] = myVector[i];
+            }
             swap(myVector, newVector);
+            delete[] newVector;
+            newVector = nullptr;
         }
     public:
-        Vector(int size_) {
+        Vector(int size_ = 0) {
             size = size_;
             myVector = new int[size];
             capacity = size;
             for(int i = 0; i < size; i++) {
-                myVector[i] = 0;
+            myVector[i] = 0;
             }
         };
         ~Vector() {
             delete[] myVector;
+            myVector = nullptr;
         };
         void print() {
             for(int i = 0; i < size; i++) {
@@ -32,13 +38,20 @@ class Vector {
         }
         void pushBack(int value) {
             if(size == capacity) {
-
+                resizeVector();
             }
+            myVector[size] = value;
+            size++;
+        }
+        int getCapacity() {
+            return capacity;
         }
 };
 
 
 int main() {
     Vector myVector(5);
+    myVector.pushBack(300);
+    cout<<myVector.getCapacity()<<endl;
     myVector.print();
 }
