a3b087498a4c5a584a8eae08be8289c0ed9cdd4a
SAL <heso482485@gmail.com>
2024-04-24 13:16:17 +0200

    added the setValue to the ArrayLL ds

--- Projects/SparseArrayAndMatrix/problem#1.cpp
+++ Projects/SparseArrayAndMatrix/problem#1.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 struct Node {
-    int data = NULL;
-    int index = NULL;
+    int data = 0;
+    int index = 0;
     Node *next = nullptr;
     Node *prev = nullptr;
     Node(int data, int index) : data(data), index(index) {}; 
@@ -14,6 +14,7 @@ class ArrayLinkedList {
         Node *head;
         Node *tail;
         int length = 0;
+
     public:
         ArrayLinkedList(int length) : head(nullptr), tail(nullptr), length(length) {};
 
@@ -25,11 +26,64 @@ class ArrayLinkedList {
             }
         };
 
-        
+        void print() { //O(n) time O(1) space
+            Node *tempHead = head;
+            while (tempHead != nullptr) {
+                cout<< tempHead->data <<endl;
+                tempHead = tempHead->next;
+            }
+        }
+
+        void setValue(int value, int index) { //O(n) time O(1) space
+            if(index > length || index < 0) {
+                return;
+            }
+
+                if(!head) {
+                    Node *newNode = new Node(value, index);
+                    head = tail = newNode;
+                    return;
+                }
+
+            for(Node *tempHead = head; tempHead; tempHead = tempHead->next) {
+                if(tempHead->index == index) {
+                    tempHead->data = value;
+                    return;
+                }
+
+                if(tempHead->index > index) {
+                    if(tempHead == head) {
+                        Node *newNode = new Node(value, index);
+                        newNode->next = tempHead;
+                        tempHead->prev = newNode;
+                        head = newNode;
+                    } else {
+                        Node *newNode = new Node(value, index);
+                        newNode->next = tempHead;
+                        newNode->prev = tempHead->prev;
+                        newNode->prev->next = newNode;
+                        tempHead->prev = newNode;
+                    }
+                    return;
+                } else if (!tempHead->next) {
+                    Node *newNode = new Node(value, index);
+                    tempHead->next = newNode;
+                    newNode->prev = tempHead;
+                    tail = newNode;
+                    return;
+                }
+            }
+        }
 
 };
 
 
 int main(void) {
-
+    ArrayLinkedList myArray(1000);
+    myArray.setValue(2, 2);
+    myArray.setValue(1, 1);
+    myArray.setValue(100, 100);
+    myArray.setValue(50, 50);
+    myArray.setValue(1000, 1000);
+    myArray.print();
 }
