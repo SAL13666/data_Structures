7c63af0cb7b36dbe083987b569be6bb69aa6df8f
SAL <heso482485@gmail.com>
2024-09-17 11:43:21 +0300

    added a clear function and made some adjustment

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -13,16 +13,39 @@ class BinaryTree {
 
         Node* root {};
 
-    public:
-        BinaryTree(int rootValue): root(new Node(rootValue)) {};
+        void clear(Node *root) { // O(n) time O(1) space
+            if(!root)
+                return;
 
-        void print(Node *node) { // O(n) time O(1) space
+            clear(root->left);
+            clear(root->right);
+            delete root;
+        }
+
+        void print(Node *node) const { // O(n) time O(1) space
             if(!node)
                 return;
 
             cout<<node->data<<" ";
             print(node->left);
             print(node->right);
+        }
+
+    public:
+        BinaryTree(int rootValue): root(new Node(rootValue)) {};
+
+        ~BinaryTree() {
+            clear(root);
+        }
+
+        void clear() { // O(n) time O(1) space
+            clear(root);
+            root = nullptr;
+        }
+
+        void print() const { // O(n) time O(1) space
+            print(root);
+            cout<< endl;
         }
 };
 
