1029975890ac737fcb823c2ca2ce4c25b994fa59
SAL <heso482485@gmail.com>
2024-11-03 22:40:49 +0200

    added the insert function

--- BinaryTree/BinarySearchTree.cpp
+++ BinaryTree/BinarySearchTree.cpp
@@ -13,7 +13,7 @@ class BinarySearchTree {
         };
         Node* root = {};
 
-        void clear(Node *root) { // O(n) time O(1) space
+        void clear(Node *root) { // O(n) time O(h) space
             if(!root)
                 return;
 
@@ -21,9 +21,41 @@ class BinarySearchTree {
             clear(root->right);
             delete root;
         }
+
+        void _insert(Node *current, int value) { // O(n) time O(h) space
+            if(value > current->data) {
+
+                if(current->right)
+                    _insert(current->right, value);
+                else
+                    current->right = new Node(value);
+
+            } else if(value < current->data) {
+
+                if(current->left)
+                    _insert(current->left, value);
+                else
+                    current->left = new Node(value);
+
+            }
+        }
     public:
         BinarySearchTree(int rootValue): root(new Node(rootValue)) {};
         ~BinarySearchTree() {
             clear(root);
         }
+
+        void insert(int value) {
+            if(!root) {
+                root = new Node(value);
+                return;
+            }
+
+            _insert(root, value);
+        }
     };
+
+
+int main() {
+    BinarySearchTree myBST(50);
+}
